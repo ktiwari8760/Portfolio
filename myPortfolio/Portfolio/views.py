487df4dd659d75f0a9a6824516f41996d11f7be8
Kartikeya Tiwari <kartikeya1.tiwari@paytm.com>
2024-02-25 15:28:45 +0530

    Adding the changes

--- myPortfolio/Portfolio/views.py
+++ myPortfolio/Portfolio/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request , 'index.html')
+    context = {'image_url': '/static/images/Myimage.jpg'}
+    return render(request , 'index.html' , context)
